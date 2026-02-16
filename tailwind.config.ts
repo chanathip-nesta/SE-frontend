643eda9ba0483cf6d5e50fa93d031d1b1ec02b99
phuwarin <phuwarin.c@kkumail.com>
2026-02-16 17:23:06 +0700

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'kku-gold': '#FFB81C',    // สีทอง มข.
+        'kku-maroon': '#8B2332',  // สีแดงเลือดหมู มข.
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
